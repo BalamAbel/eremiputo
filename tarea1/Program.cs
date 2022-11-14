8950ed5d8db98f15227b37cac2e5b20e0b078e4b
BalamAbel <117304698+BalamAbel@users.noreply.github.com>
2022-11-13 21:17:30 -0600

    Add files via upload

---
+++ tarea1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace TAREA1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            
+            
+           
+        }
+        
+        public int algoRecursivo (int N)
+        {
+            Console.WriteLine("dame un numero ");
+            int dato = int.Parse(Console.ReadLine());
+
+            if (N >0)
+            {
+                return algoRecursivo(N-1);
+            }
+            else 
+            {
+                return 0;
+                Console.w
+            }
+
+        }
+    }
+}
